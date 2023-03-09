87ea1029e51dbe715f90c955884682d2f6a79ef8
Charles Karlo Leguarda <charlesleguarda@gmail.com>
2023-03-09 11:50:16 +0800

    First commit

---
+++ components/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='w-full md:h-screen p-2 flex items-center py-16'>
+        <div className='max-w-[1240px] m-auto md:grid grid-col-3 gap-8'>
+            <div className='col-span-2'>
+                <p>About</p>
+                <h2>Who I Am</h2>
+                <p>// I am not your normal developer</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla dignissimos voluptas ipsum quaerat eligendi illum. Est omnis numquam voluptas officia eligendi, beatae ipsa facilis libero nesciunt eaque aliquid suscipit, nobis voluptatum, aut maxime? Sunt sint numquam quos mollitia impedit pariatur autem, sequi fugit aperiam, architecto recusandae at in eveniet laboriosam totam illo doloremque magni fuga voluptatum culpa, molestiae omnis consectetur. Molestias consequatur suscipit corrupti adipisci eaque illo quae fugit aliquam recusandae architecto? Praesentium officia soluta, ipsa voluptate ab quam accusamus aperiam quasi sequi enim eos maxime nisi, corporis, necessitatibus sunt temporibus optio qui harum a ea fuga libero. Et, voluptas.</p>
+                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint autem ex totam modi atque dolore dolores sunt obcaecati sit sapiente. Alias eaque quibusdam incidunt odio maxime nisi, ipsa asperiores ad quaerat distinctio esse laudantium molestias ducimus laboriosam nam vitae! Ipsam dolore fugiat hic. Natus soluta commodi eos, aliquam nesciunt dolores eveniet dolorum omnis ducimus reiciendis vel! Tenetur magnam labore recusandae doloribus soluta delectus vitae accusantium neque numquam? Quisquam molestias mollitia sunt ea maiores inventore quod iste alias nobis et! Fugiat voluptate quae ipsa, eum, similique quam minima id nihil dolores dicta repellendus blanditiis? Doloremque, aut? Perferendis reiciendis officia veritatis architecto.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
